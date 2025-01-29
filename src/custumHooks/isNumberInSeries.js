ec19af32e28f95bee8fa2c0b270ad152939b946f
Yograj Singh <ryog7910@gmail.com>
2025-01-28 17:44:50 -1100

    Automated commit

--- src/custumHooks/isNumberInSeries.js
+++ src/custumHooks/isNumberInSeries.js
@@ -1,23 +1,23 @@
 const isNumberInSeries = ( num ) => {
-    let current = 0; // Series starts from 0
-    let step = 3;    // Step for the first number (3rd after each pair)
+    let current = 0; 
+    let step = 3;    
 
     while (current <= num) {
-      // Condition 1: Check if the current number is equal to the input number
+      // Check if the current number is equal to the input number
       if (current === num) {
         return true;
       }
       // After adding the first number, we add the second consecutive number
-      current += step; // Increment current by 3 for the first number
+      current += step; 
       if (current === num) {
         return true;
       }
-      current += 1;     // Add the next consecutive number
+      current += 1;
 
       // Reset step to 3 for the next iteration
       step = 3;
     }
-    return false;  // If number is not found in the series
+    return false;  
   };
 
   export default isNumberInSeries;
