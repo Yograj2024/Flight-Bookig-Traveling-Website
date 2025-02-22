53b08297118e575214908db7b12e3cc64a957bf3
Yograj Singh <ryog7910@gmail.com>
2025-02-22 10:48:54 -1100

    Automated commit

---
+++ nav.js
@@ -0,0 +1,11 @@
+[ 
+    {
+      "navHeading":["Home","About Us","Services","Best Deals","Destination","Contact Us"]
+    },
+    {
+      "name": "John Doe",
+      "age": 30,
+      "city": "New York"
+    }
+  ]
+  
