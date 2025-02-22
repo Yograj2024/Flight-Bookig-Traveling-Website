6fc1920ec493809f3e0d177bf7e5412da59ea319
Yograj Singh <ryog7910@gmail.com>
2025-02-22 11:54:07 -1100

    Automated commit

--- src/utils/nav.js
+++ src/utils/nav.js
@@ -2,5 +2,60 @@
     {
         "name":"yogi",
         "no":98767364
-    }
+    },{   
+        "airportInfo": {
+            "name": "Indira Gandhi International Airport",
+            "type": "International",
+            "terminalCounts": "3", 
+            "iataCode": "DEL",
+            "shortDescription": "Indira Gandhi International Airport is the primary international gateway to India, serving as a major hub for both domestic and international flights.",
+            "runway_length": "4,430 meters",
+            "terminal_count": 3,
+            "passenger_capacity": "70 million/year",
+        },
+        "timeZone": "IST (Indian Standard Time)",
+        "cityDescription": "Delhi, the capital of India, is a bustling metropolis blending ancient history with modern urban developments. Known for its rich culture, historic monuments, and vibrant markets, it is one of the most important cities in India.",
+        "facilities": ["Free WiFi", "Duty-Free Shops", "Lounges", "Baggage Services", "Medical Assistance"],
+        "location": {
+            "country": "India",
+            city: "Delhi",
+            "latitude": 28.5562,
+            "longitude": 77.1000
+        },
+        "contact": {
+            "phone": "+91 11 2565 8450",
+            "email": "delhi.airport@aai.aero"
+        },
+        "weather": {
+            "current": {
+                "temperature": 4.5,
+                "condition": "Partly Cloudy",
+                "wind_speed": 16,
+                "humidity": 78
+            },
+            "forecast": {
+                "temperature": 5.0,
+                "condition": "Cloudy",
+                "wind_speed": 13,
+                "humidity": 73
+            }
+        },
+        "transportation": [
+            {
+                "type": "Taxi",
+                "availability": true,
+                "contact": "Dial 100 for airport taxis."
+            },
+            {
+                "type": "Airport Shuttle",
+                "availability": true,
+                "contact": "Available at the terminal entrances."
+            },
+            {
+                "type": "Metro",
+                "availability": true,
+                "contact": "Airport Express Line (Delhi Metro) - Connects directly to the city."
+            }
+        ]
+    },
 ]
